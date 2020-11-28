b7ecd8fbf80b09a9d930d93ef561b068caa572d2
chenshijie <chenshijie@uniontech.com>
2020-11-28 15:22:27 -0800

    right clang-format

--- src/lib/configwidgetslib/erroroverlay.cpp
+++ src/lib/configwidgetslib/erroroverlay.cpp
@@ -17,74 +17,74 @@ namespace kcm {
 ErrorOverlay::ErrorOverlay(DBusProvider *dbus, QWidget *parent)
     : QWidget(parent), ui_(std::make_unique<Ui::ErrorOverlay>()),
       baseWidget_(parent) {
-  ui_->setupUi(this);
-  setVisible(false);
+    ui_->setupUi(this);
+    setVisible(false);
 
-  baseWidget_->installEventFilter(this);
-  ui_->pixmapLabel->setPixmap(
-      QIcon::fromTheme("dialog-error-symbolic").pixmap(64));
+    baseWidget_->installEventFilter(this);
+    ui_->pixmapLabel->setPixmap(
+        QIcon::fromTheme("dialog-error-symbolic").pixmap(64));
 
-  connect(baseWidget_, &QObject::destroyed, this, &QObject::deleteLater);
-  connect(dbus, &DBusProvider::availabilityChanged, this,
-          &ErrorOverlay::availabilityChanged);
+    connect(baseWidget_, &QObject::destroyed, this, &QObject::deleteLater);
+    connect(dbus, &DBusProvider::availabilityChanged, this,
+            &ErrorOverlay::availabilityChanged);
 
-  connect(ui_->runfcitxButton, &QAbstractButton::pressed, this,
-          &ErrorOverlay::runFcitx5);
-  availabilityChanged(dbus->available());
+    connect(ui_->runfcitxButton, &QAbstractButton::pressed, this,
+            &ErrorOverlay::runFcitx5);
+    availabilityChanged(dbus->available());
 }
 
 ErrorOverlay::~ErrorOverlay() {}
 
 void ErrorOverlay::availabilityChanged(bool avail) {
-  const bool newEnabled = !avail;
-  if (enabled_ != newEnabled) {
-    enabled_ = newEnabled;
-    setVisible(newEnabled);
-    if (newEnabled) {
-      reposition();
+    const bool newEnabled = !avail;
+    if (enabled_ != newEnabled) {
+        enabled_ = newEnabled;
+        setVisible(newEnabled);
+        if (newEnabled) {
+            reposition();
+        }
     }
-  }
 }
 
 void ErrorOverlay::runFcitx5() {
-  QProcess::startDetached(
-      QString::fromStdString(StandardPath::fcitxPath("bindir", "fcitx5")),
-      QStringList());
+    QProcess::startDetached(
+        QString::fromStdString(StandardPath::fcitxPath("bindir", "fcitx5")),
+        QStringList());
 }
 
 void ErrorOverlay::reposition() {
-  if (!baseWidget_) {
-    return;
-  }
+    if (!baseWidget_) {
+        return;
+    }
 
-  // follow base widget visibility
-  // needed eg. in tab widgets
-  if (!baseWidget_->isVisible()) {
-    hide();
-    return;
-  }
+    // follow base widget visibility
+    // needed eg. in tab widgets
+    if (!baseWidget_->isVisible()) {
+        hide();
+        return;
+    }
 
-  show();
+    show();
 
-  // follow position changes
-  const QPoint topLevelPos = baseWidget_->mapTo(window(), QPoint(0, 0));
-  const QPoint parentPos = parentWidget()->mapFrom(window(), topLevelPos);
-  move(parentPos);
+    // follow position changes
+    const QPoint topLevelPos = baseWidget_->mapTo(window(), QPoint(0, 0));
+    const QPoint parentPos = parentWidget()->mapFrom(window(), topLevelPos);
+    move(parentPos);
 
-  // follow size changes
-  // TODO: hide/scale icon if we don't have enough space
-  resize(baseWidget_->size());
-  raise();
+    // follow size changes
+    // TODO: hide/scale icon if we don't have enough space
+    resize(baseWidget_->size());
+    raise();
 }
 
 bool ErrorOverlay::eventFilter(QObject *object, QEvent *event) {
-  if (enabled_ && object == baseWidget_ &&
-      (event->type() == QEvent::Move || event->type() == QEvent::Resize ||
-       event->type() == QEvent::Show || event->type() == QEvent::Hide ||
-       event->type() == QEvent::ParentChange)) {
-    reposition();
-  }
-  return QWidget::eventFilter(object, event);
+    if (enabled_ && object == baseWidget_ &&
+        (event->type() == QEvent::Move || event->type() == QEvent::Resize ||
+         event->type() == QEvent::Show || event->type() == QEvent::Hide ||
+         event->type() == QEvent::ParentChange)) {
+        reposition();
+    }
+    return QWidget::eventFilter(object, event);
 }
 
 } // namespace kcm
