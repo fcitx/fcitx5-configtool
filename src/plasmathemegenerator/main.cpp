cf8ec27dfecd6773fa4e7d398f2f9a6544576841
Weng Xuetian <wengxt@gmail.com>
2022-07-02 17:23:36 -0700

    Add a monitor mode for plasma theme generator to be used by classic ui Addon

--- src/plasmathemegenerator/main.cpp
+++ src/plasmathemegenerator/main.cpp
@@ -11,12 +11,17 @@
 #include <QDebug>
 #include <QDir>
 #include <QPainter>
+#include <QSocketNotifier>
 #include <fcitx-config/iniparser.h>
 #include <fcitx-config/rawconfig.h>
 #include <fcitx-utils/color.h>
 #include <fcitx-utils/i18n.h>
+#include <fcntl.h>
 #include <memory>
 #include <qmath.h>
+
+namespace {
+bool fd_is_valid(int fd) { return fcntl(fd, F_GETFD) != -1 || errno != EBADF; }
 
 fcitx::Color toFcitxColor(const QColor &color) {
     fcitx::Color fcitxColor;
@@ -36,189 +41,281 @@ void setMarginsToConfig(fcitx::RawConfig &config, const std::string &name,
     subConfig["Bottom"] = std::to_string(qRound(bottom));
 }
 
+} // namespace
+
+class WatcherApp : public QGuiApplication {
+    Q_OBJECT
+public:
+    WatcherApp(int &argc, char **argv) : QGuiApplication(argc, argv) {
+        setApplicationName(QLatin1String("fcitx5-plasma-theme-watcher"));
+        setApplicationVersion(QLatin1String(PROJECT_VERSION));
+    }
+
+    bool init() {
+        QCommandLineParser parser;
+        parser.setApplicationDescription(
+            _("Generate Fcitx 5 Classic UI Theme based on Plasma theme"));
+        parser.addHelpOption();
+        parser.addOptions(
+            {{{"t", "theme"}, _("Plasma theme name <name> "), _("name")},
+             {{"o", "output"}, _("Output path <output> "), _("output")}});
+        QCommandLineOption option{"fd", _("File descriptor <fd> "), _("fd")};
+        option.setFlags(QCommandLineOption::HiddenFromHelp);
+        parser.addOption(option);
+        parser.process(*this);
+
+        if (parser.isSet("fd")) {
+            int fd = -1;
+            bool ok = false;
+            fd = parser.value("fd").toInt(&ok);
+            if (!ok || !fd_is_valid(fd)) {
+                return false;
+            }
+            fd_ = fd;
+        }
+
+        if (parser.isSet("output")) {
+            outputPath_ = parser.value("output");
+        } else {
+            outputPath_ = QString::fromStdString(fcitx::stringutils::joinPath(
+                fcitx::StandardPath::global().userDirectory(
+                    fcitx::StandardPath::Type::PkgData),
+                "themes/plasma"));
+        }
+
+        qDebug() << "Will write new themes to: " << outputPath_;
+
+        if (parser.isSet("theme")) {
+            theme_ = std::make_unique<Plasma::Theme>(parser.value("theme"));
+        } else {
+            theme_ = std::make_unique<Plasma::Theme>();
+        }
+
+        if (monitorMode()) {
+            socketNotifier_ =
+                new QSocketNotifier(fd_, QSocketNotifier::Read, this);
+            connect(socketNotifier_, &QSocketNotifier::activated, this,
+                    [this]() {
+                        char buf;
+                        if (fcitx::fs::safeRead(fd_, &buf, 1) <= 0) {
+                            quit();
+                        }
+                    });
+            connect(theme_.get(), &Plasma::Theme::themeChanged, this, [this]() {
+                if (!generateTheme()) {
+                    qDebug() << "Failed to generate theme.";
+                }
+            });
+        }
+        return true;
+    }
+
+    bool monitorMode() const { return fd_ >= 0; }
+
+    bool generateTheme() {
+        QDir dir(outputPath_);
+        if (!dir.mkpath(".")) {
+            return false;
+        }
+        // Same logic from plasma-frameworks
+        const int gridUnit = QFontMetrics(QGuiApplication::font())
+                                 .boundingRect(QStringLiteral("M"))
+                                 .height();
+        const int smallSpacing =
+            qMax(2, (gridUnit / 4)); // 1/4 of gridUnit, at least 2
+        const qreal textMargin = smallSpacing / 2.0f;
+        fcitx::RawConfig config;
+        // Write metadata to theme config
+        auto &metadata = config["Metadata"];
+        metadata["Name"] = "Plasma";
+        metadata["Version"] = "1";
+        metadata["Author"] = "Fcitx";
+        metadata["Description"] =
+            QString(_("Theme generated from Plasma Theme %1"))
+                .arg(theme_->themeName())
+                .toStdString();
+
+        auto &inputPanel = config["InputPanel"];
+        inputPanel["NormalColor"] =
+            toFcitxColor(theme_->color(Plasma::Theme::TextColor)).toString();
+        inputPanel["HighlightCandidateColor"] =
+            toFcitxColor(theme_->color(Plasma::Theme::TextColor)).toString();
+        inputPanel["HighlightColor"] =
+            toFcitxColor(theme_->color(Plasma::Theme::HighlightedTextColor))
+                .toString();
+        inputPanel["HighlightBackgroundColor"] =
+            toFcitxColor(theme_->color(Plasma::Theme::HighlightColor))
+                .toString();
+
+        auto &menu = config["Menu"];
+        inputPanel["NormalColor"] =
+            toFcitxColor(theme_->color(Plasma::Theme::TextColor)).toString();
+        inputPanel["HighlightCandidateColor"] =
+            toFcitxColor(theme_->color(Plasma::Theme::TextColor)).toString();
+
+        QImage background(QSize(200, 200), QImage::Format_ARGB32);
+        background.fill(Qt::transparent);
+
+        {
+            qreal shadowLeft = 0, shadowRight = 0, shadowTop = 0,
+                  shadowBottom = 0;
+            qreal bgLeft = 0, bgRight = 0, bgTop = 0, bgBottom = 0;
+            Plasma::FrameSvg shadowSvg;
+            shadowSvg.setTheme(theme_.get());
+            shadowSvg.setImagePath("dialogs/background");
+            if (shadowSvg.hasElementPrefix("shadow")) {
+                shadowSvg.setElementPrefix("shadow");
+                shadowSvg.resizeFrame(QSize(200, 200));
+                shadowSvg.getMargins(shadowLeft, shadowTop, shadowRight,
+                                     shadowBottom);
+            }
+
+            Plasma::FrameSvg svg;
+            svg.setTheme(theme_.get());
+            svg.setImagePath("dialogs/background");
+            svg.resizeFrame(
+                QSizeF(200, 200) -
+                QSizeF(shadowLeft + shadowRight, shadowTop + shadowBottom));
+            svg.getMargins(bgLeft, bgTop, bgRight, bgBottom);
+            {
+                QPainter p(&background);
+                p.setRenderHint(QPainter::SmoothPixmapTransform);
+                svg.paintFrame(&p, QPointF(shadowLeft, shadowTop));
+                shadowSvg.paintFrame(&p);
+                p.end();
+            }
+            bgLeft += shadowLeft;
+            bgTop += shadowTop;
+            bgRight += shadowRight;
+            bgBottom += shadowBottom;
+            if (!background.save(dir.filePath("panel.png"))) {
+                return false;
+            }
+
+            menu["Spacing"] = std::to_string(textMargin);
+            setMarginsToConfig(inputPanel, "ContentMargin", bgLeft, bgTop,
+                               bgRight, bgBottom);
+            setMarginsToConfig(menu, "ContentMargin", bgLeft, bgTop, bgRight,
+                               bgBottom);
+            setMarginsToConfig(inputPanel, "ShadowMargin", shadowLeft,
+                               shadowTop, shadowRight, shadowBottom);
+            inputPanel["Background"]["Image"] = "panel.png";
+            menu["Background"]["Image"] = "panel.png";
+            setMarginsToConfig(inputPanel["Background"], "Margin", bgLeft,
+                               bgTop, bgRight, bgBottom);
+            setMarginsToConfig(menu["Background"], "Margin", bgLeft, bgTop,
+                               bgRight, bgBottom);
+        }
+
+        {
+            Plasma::FrameSvg highlightSvg;
+            highlightSvg.setTheme(theme_.get());
+            highlightSvg.setImagePath("widgets/viewitem");
+            if (highlightSvg.hasElementPrefix("hover")) {
+                highlightSvg.setElementPrefix("hover");
+            } else if (highlightSvg.hasElementPrefix("selected")) {
+                highlightSvg.setElementPrefix("selected");
+            }
+            highlightSvg.resizeFrame(QSize(200, 200));
+            if (!highlightSvg.framePixmap().save(
+                    dir.filePath("highlight.png"))) {
+                return false;
+            }
+            qreal bgLeft = 0, bgRight = 0, bgTop = 0, bgBottom = 0;
+            highlightSvg.getMargins(bgLeft, bgTop, bgRight, bgBottom);
+            bgLeft = qMax(textMargin, bgLeft);
+            bgTop = qMax(textMargin, bgTop);
+            bgRight = qMax(textMargin, bgRight);
+            bgBottom = qMax(textMargin, bgBottom);
+
+            inputPanel["Highlight"]["Image"] = "highlight.png";
+            menu["Highlight"]["Image"] = "highlight.png";
+            setMarginsToConfig(inputPanel["Highlight"], "Margin", bgLeft, bgTop,
+                               bgRight, bgBottom);
+            setMarginsToConfig(menu["Highlight"], "Margin", bgLeft, bgTop,
+                               bgRight, bgBottom);
+            setMarginsToConfig(inputPanel, "TextMargin", bgLeft,
+                               bgTop + textMargin, bgRight,
+                               bgBottom + textMargin);
+            setMarginsToConfig(menu, "TextMargin", bgLeft, bgTop, bgRight,
+                               bgBottom);
+        }
+
+        {
+            Plasma::Svg icon;
+            icon.setTheme(theme_.get());
+            icon.setImagePath("widgets/arrows");
+            if (icon.hasElement("left-arrow") &&
+                icon.hasElement("right-arrow")) {
+                inputPanel["PrevPage/Image"] = "prev.png";
+                if (!icon.pixmap("left-arrow").save(dir.filePath("prev.png"))) {
+                    return false;
+                }
+                inputPanel["NextPage/Image"] = "next.png";
+                if (!icon.pixmap("right-arrow")
+                         .save(dir.filePath("next.png"))) {
+                    return false;
+                }
+            }
+            if (icon.hasElement("right-arrow")) {
+                menu["SubMenu/Image"] = "arrow.png";
+                if (!icon.pixmap("right-arrow")
+                         .save(dir.filePath("arrow.png"))) {
+                    return false;
+                }
+            }
+
+            Plasma::Svg radio;
+            radio.setTheme(theme_.get());
+            radio.setImagePath("widgets/checkmarks");
+            if (radio.hasElement("radiobutton")) {
+                menu["CheckBox/Image"] = "radio.png";
+                if (!radio.pixmap("radiobutton")
+                         .save(dir.filePath("radio.png"))) {
+                    return false;
+                }
+            }
+            Plasma::Svg line;
+            line.setTheme(theme_.get());
+            line.setImagePath("widgets/line");
+            if (line.hasElement("horizontal-line")) {
+                if (!line.pixmap("horizontal-line")
+                         .save(dir.filePath("line.png"))) {
+                    return false;
+                }
+                menu["Separator/Image"] = "line.png";
+            }
+        }
+
+        if (monitorMode()) {
+            char buf = 0;
+            fcitx::fs::safeWrite(fd_, &buf, 1);
+            qDebug() << "Notify theme reloading.";
+        }
+        return fcitx::safeSaveAsIni(
+            config, dir.filePath("theme.conf").toLocal8Bit().constData());
+    }
+
+private:
+    QSocketNotifier *socketNotifier_ = nullptr;
+    int fd_ = -1;
+    std::unique_ptr<Plasma::Theme> theme_;
+    QString outputPath_;
+};
+
 int main(int argc, char *argv[]) {
-    QGuiApplication app(argc, argv);
+    WatcherApp app(argc, argv);
     app.setApplicationName(QLatin1String("fcitx5-plasma-theme-generator"));
     app.setApplicationVersion(QLatin1String(PROJECT_VERSION));
-    QCommandLineParser parser;
-    parser.setApplicationDescription(
-        _("Generate Fcitx 5 Classic UI Theme based on Plasma theme"));
-    parser.addHelpOption();
-    parser.addOptions(
-        {{{"t", "theme"}, _("Plasma theme name <name> "), _("name")},
-         {{"o", "output"}, _("Output path <output> "), _("output")}});
-    parser.process(app);
-
-    QString outputPath;
-
-    if (parser.isSet("output")) {
-        outputPath = parser.value("output");
-    } else {
-        outputPath = QString::fromStdString(fcitx::stringutils::joinPath(
-            fcitx::StandardPath::global().userDirectory(
-                fcitx::StandardPath::Type::PkgData),
-            "themes/plasma"));
+    if (!app.init()) {
+        return 1;
     }
 
-    qDebug() << "Will write new themes to: " << outputPath;
-
-    QDir dir(outputPath);
-    dir.mkpath(".");
-
-    // Same logic from plasma-frameworks
-    const int gridUnit = QFontMetrics(QGuiApplication::font())
-                             .boundingRect(QStringLiteral("M"))
-                             .height();
-    const int smallSpacing =
-        qMax(2, (gridUnit / 4)); // 1/4 of gridUnit, at least 2
-    const qreal textMargin = smallSpacing / 2.0f;
-    std::unique_ptr<Plasma::Theme> theme;
-    if (parser.isSet("theme")) {
-        theme = std::make_unique<Plasma::Theme>(parser.value("theme"));
-    } else {
-        theme = std::make_unique<Plasma::Theme>();
+    auto ret = app.generateTheme();
+    if (!app.monitorMode()) {
+        return ret ? 0 : 1;
     }
-    qDebug() << "Plasma Theme name:" << theme->themeName();
-    fcitx::RawConfig config;
-    // Write metadata to theme config
-    auto &metadata = config["Metadata"];
-    metadata["Name"] = "Plasma";
-    metadata["Version"] = "1";
-    metadata["Author"] = "Fcitx";
-    metadata["Description"] = QString(_("Theme generated from Plasma Theme %1"))
-                                  .arg(theme->themeName())
-                                  .toStdString();
-
-    auto &inputPanel = config["InputPanel"];
-    inputPanel["NormalColor"] =
-        toFcitxColor(theme->color(Plasma::Theme::TextColor)).toString();
-    inputPanel["HighlightCandidateColor"] =
-        toFcitxColor(theme->color(Plasma::Theme::TextColor)).toString();
-    inputPanel["HighlightColor"] =
-        toFcitxColor(theme->color(Plasma::Theme::HighlightedTextColor))
-            .toString();
-    inputPanel["HighlightBackgroundColor"] =
-        toFcitxColor(theme->color(Plasma::Theme::HighlightColor)).toString();
-
-    auto &menu = config["Menu"];
-    inputPanel["NormalColor"] =
-        toFcitxColor(theme->color(Plasma::Theme::TextColor)).toString();
-    inputPanel["HighlightCandidateColor"] =
-        toFcitxColor(theme->color(Plasma::Theme::TextColor)).toString();
-
-    QImage background(QSize(200, 200), QImage::Format_ARGB32);
-    background.fill(Qt::transparent);
-
-    {
-        qreal shadowLeft = 0, shadowRight = 0, shadowTop = 0, shadowBottom = 0;
-        qreal bgLeft = 0, bgRight = 0, bgTop = 0, bgBottom = 0;
-        Plasma::FrameSvg shadowSvg;
-        shadowSvg.setTheme(theme.get());
-        shadowSvg.setImagePath("dialogs/background");
-        if (shadowSvg.hasElementPrefix("shadow")) {
-            shadowSvg.setElementPrefix("shadow");
-            shadowSvg.resizeFrame(QSize(200, 200));
-            shadowSvg.getMargins(shadowLeft, shadowTop, shadowRight,
-                                 shadowBottom);
-        }
-
-        Plasma::FrameSvg svg;
-        svg.setTheme(theme.get());
-        svg.setImagePath("dialogs/background");
-        svg.resizeFrame(QSizeF(200, 200) - QSizeF(shadowLeft + shadowRight,
-                                                  shadowTop + shadowBottom));
-        svg.getMargins(bgLeft, bgTop, bgRight, bgBottom);
-        {
-            QPainter p(&background);
-            p.setRenderHint(QPainter::SmoothPixmapTransform);
-            svg.paintFrame(&p, QPointF(shadowLeft, shadowTop));
-            shadowSvg.paintFrame(&p);
-            p.end();
-        }
-        bgLeft += shadowLeft;
-        bgTop += shadowTop;
-        bgRight += shadowRight;
-        bgBottom += shadowBottom;
-        background.save(dir.filePath("panel.png"));
-
-        menu["Spacing"] = std::to_string(textMargin);
-        setMarginsToConfig(inputPanel, "ContentMargin", bgLeft, bgTop, bgRight,
-                           bgBottom);
-        setMarginsToConfig(menu, "ContentMargin", bgLeft, bgTop, bgRight,
-                           bgBottom);
-        setMarginsToConfig(inputPanel, "ShadowMargin", shadowLeft, shadowTop,
-                           shadowRight, shadowBottom);
-        inputPanel["Background"]["Image"] = "panel.png";
-        menu["Background"]["Image"] = "panel.png";
-        setMarginsToConfig(inputPanel["Background"], "Margin", bgLeft, bgTop,
-                           bgRight, bgBottom);
-        setMarginsToConfig(menu["Background"], "Margin", bgLeft, bgTop, bgRight,
-                           bgBottom);
-    }
-
-    {
-        Plasma::FrameSvg highlightSvg;
-        highlightSvg.setTheme(theme.get());
-        highlightSvg.setImagePath("widgets/viewitem");
-        if (highlightSvg.hasElementPrefix("hover")) {
-            highlightSvg.setElementPrefix("hover");
-        } else if (highlightSvg.hasElementPrefix("selected")) {
-            highlightSvg.setElementPrefix("selected");
-        }
-        highlightSvg.resizeFrame(QSize(200, 200));
-        highlightSvg.framePixmap().save(dir.filePath("highlight.png"));
-        qreal bgLeft = 0, bgRight = 0, bgTop = 0, bgBottom = 0;
-        highlightSvg.getMargins(bgLeft, bgTop, bgRight, bgBottom);
-        bgLeft = qMax(textMargin, bgLeft);
-        bgTop = qMax(textMargin, bgTop);
-        bgRight = qMax(textMargin, bgRight);
-        bgBottom = qMax(textMargin, bgBottom);
-
-        inputPanel["Highlight"]["Image"] = "highlight.png";
-        menu["Highlight"]["Image"] = "highlight.png";
-        setMarginsToConfig(inputPanel["Highlight"], "Margin", bgLeft, bgTop,
-                           bgRight, bgBottom);
-        setMarginsToConfig(menu["Highlight"], "Margin", bgLeft, bgTop, bgRight,
-                           bgBottom);
-        setMarginsToConfig(inputPanel, "TextMargin", bgLeft, bgTop + textMargin,
-                           bgRight, bgBottom + textMargin);
-        setMarginsToConfig(menu, "TextMargin", bgLeft, bgTop, bgRight,
-                           bgBottom);
-    }
-
-    {
-        Plasma::Svg icon;
-        icon.setTheme(theme.get());
-        icon.setImagePath("widgets/arrows");
-        if (icon.hasElement("left-arrow") && icon.hasElement("right-arrow")) {
-            inputPanel["PrevPage/Image"] = "prev.png";
-            icon.pixmap("left-arrow").save(dir.filePath("prev.png"));
-            inputPanel["NextPage/Image"] = "next.png";
-            icon.pixmap("right-arrow").save(dir.filePath("next.png"));
-        }
-        if (icon.hasElement("right-arrow")) {
-            menu["SubMenu/Image"] = "arrow.png";
-            icon.pixmap("right-arrow").save(dir.filePath("arrow.png"));
-        }
-
-        Plasma::Svg radio;
-        radio.setTheme(theme.get());
-        radio.setImagePath("widgets/checkmarks");
-        if (radio.hasElement("radiobutton")) {
-            menu["CheckBox/Image"] = "radio.png";
-            radio.pixmap("radiobutton").save(dir.filePath("radio.png"));
-        }
-        Plasma::Svg line;
-        line.setTheme(theme.get());
-        line.setImagePath("widgets/line");
-        if (line.hasElement("horizontal-line")) {
-            line.pixmap("horizontal-line").save(dir.filePath("line.png"));
-            menu["Separator/Image"] = "line.png";
-        }
-    }
-
-    return fcitx::safeSaveAsIni(config, fcitx::StandardPath::Type::PkgData,
-                                "themes/plasma/theme.conf")
-               ? 0
-               : 1;
+    return app.exec();
 }
+
+#include "main.moc"
