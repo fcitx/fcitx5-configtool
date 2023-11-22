f3d9b83a2e3f47a3cba7c85c5dc1f2cd020338f6
Weng Xuetian <wengxt@gmail.com>
2023-11-21 21:19:45 -0800

    fix kcm discovery under KDE.
    desktop under kservices5 and applications should not co-exists. It will
    break the kcm lookup if both presents.
    
    Check against the KF5 version to use the new install namespace.

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gen_pot kde:appdata:desktop:ui fcitx5-configtool po/fcitx5-configtool layout/ src/configtool/ src/lib/ src/migrator/
-gen_pot kde:appdata:desktop:ui kcm_fcitx5 po/org.fcitx.fcitx5.kcm src/kcm/ src/plasmathemegenerator/
+gen_pot kde:appdata:desktop:ui kcm_fcitx5 po/kcm_fcitx5 src/kcm/ src/plasmathemegenerator/
