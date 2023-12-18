3d02807eaf7f74a37074d74092aa6cb7a5471d79
ck <chenke@deepin.org>
2023-12-18 11:57:23 +0800

    Loading Qt translations
    e.g. QLineEdit contextMenu, QColorDialog

--- src/configtool/main.cpp
+++ src/configtool/main.cpp
@@ -6,6 +6,7 @@
  */
 #include "config.h"
 #include "mainwindow.h"
+#include "logging.h"
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
@@ -15,6 +16,23 @@ int main(int argc, char *argv[]) {
     app.setApplicationDisplayName(_("Fcitx Configuration"));
     app.setOrganizationDomain("fcitx.org");
     fcitx::registerFcitxQtDBusTypes();
+
+    const QString &localName = QLocale::system().name();
+    const QString &transPath = QLibraryInfo::location(QLibraryInfo::TranslationsPath);
+    QStringList qtTransFiles = { transPath + QString("/qt_") + localName,
+                               transPath + QString("/qtbase_") + localName };
+    for (const QString &filename : qtTransFiles) {
+        QTranslator *translator = new QTranslator;
+        bool ret = translator->load(filename);
+        qCDebug(KCM_FCITX5) <<  "Loading translator" << filename;
+
+        if (!ret) {
+            delete translator;
+            continue;
+        }
+
+        app.installTranslator(translator);
+    }
 
     fcitx::kcm::MainWindow mainWindow;
     mainWindow.show();
