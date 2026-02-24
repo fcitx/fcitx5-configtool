8e291f71b3208610fcb6ebca0d6fb9e1bb7a2ece
Weng Xuetian <wengxt@gmail.com>
2026-02-24 01:37:12 -0800

    Update Message.sh

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 gen_pot kde:appdata:desktop:ui fcitx5-configtool po/fcitx5-configtool layout/ src/configtool/ src/lib/ src/migrator/
-gen_pot kde:appdata:desktop:ui kcm_fcitx5 po/kcm_fcitx5 src/kcm/ src/plasmathemegenerator/
+gen_pot kde:json:desktop:ui kcm_fcitx5 po/kcm_fcitx5 src/kcm/ src/plasmathemegenerator/
 
-gen_json kcm_fcitx5 po/kcm_fcitx5 src/kcm/kcm_fcitx5.json
+gen_json po/kcm_fcitx5 src/kcm/kcm_fcitx5.json
