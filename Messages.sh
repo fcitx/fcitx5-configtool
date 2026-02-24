a5c2178d29d1ae01d3f48381ce852ebfa9aca002
Weng Xuetian <wengxt@gmail.com>
2026-02-23 19:06:44 -0800

    Add new extract_json command for update desktop style json

--- Messages.sh
+++ Messages.sh
@@ -2,3 +2,5 @@
 
 gen_pot kde:appdata:desktop:ui fcitx5-configtool po/fcitx5-configtool layout/ src/configtool/ src/lib/ src/migrator/
 gen_pot kde:appdata:desktop:ui kcm_fcitx5 po/kcm_fcitx5 src/kcm/ src/plasmathemegenerator/
+
+gen_json kcm_fcitx5 po/kcm_fcitx5 src/kcm_fcitx5.json
